14a0eb30894f2abebd6d7d5985f71f6fb76e8329
David Benkeser <benkeser@emory.edu>
2021-03-03 17:16:57 +0000

    update the test build reports.

--- _build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Building bookdown $1 report"
-Rscript -e "bookdown::clean_book(TRUE)"
-Rscript -e "bookdown::render_book(input = 'index.Rmd', config_file = '_bookdown_$1.yml', output_format = 'bookdown::pdf_document2', quiet=TRUE)"
